79e83933cdcb0a4850758fc2b295701d3fe1b8ec
Sydney J <sjennings16@my.bcit.ca>
2025-03-11 14:30:54 -0700

    deleted unused images

---
+++ .history/scripts/config_20250311143053.js
@@ -0,0 +1,7 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyDusEPSeKh9n2I4KCw5cI3CLg1MDUE3Rsk",
+    authDomain: "streetsmartbcit.firebaseapp.com",
+    projectId: "streetsmartbcit",
+    storageBucket: "streetsmartbcit.appspot.com",
+}
